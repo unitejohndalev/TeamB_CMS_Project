41881b1bae561a506bcb328053c6cd8260e8a2e1
dale <unitejohndalev@gmail.com>
2024-01-10 09:31:55 +0800

    Modify Personal Info

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,17 +1,27 @@
-
-
 const PersonalInfo = () => {
   return (
     <>
-      <div className="w-[90%] flex flex-col m-auto h-[50vh] shadow-lg">
+      <div className="w-[90%] flex flex-col m-auto h-full shadow-lg">
         <form className="flex flex-col h-full gap-y-5 w-[90%] m-auto">
           <img src="" alt="" />
-          <input type="text" className="input-style"  placeholder="First Name*"/>
-          <input type="text" className="input-style"  placeholder="Last Name*"/>
-          <input type="email" className="input-style" placeholder="Email Address*" />
-          <input type="text" className="input-style"  placeholder="Contact Number*"/>
+          <input
+            type="text"
+            className="input-style"
+            placeholder="First Name*"
+          />
+          <input type="text" className="input-style" placeholder="Last Name*" />
+          <input
+            type="email"
+            className="input-style"
+            placeholder="Email Address*"
+          />
+          <input
+            type="text"
+            className="input-style"
+            placeholder="Contact Number*"
+          />
+        <button className="mb-5 btn-style w-full m-auto">Update</button>
         </form>
-        <button className="mb-5 btn-style w-[90%] m-auto">Update</button>
       </div>
     </>
   );
