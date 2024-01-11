25039d14fb8cd21480fedb00f5c112971effdd19
Gem Ramirez <geraldinemaesramirez@gmail.com>
2024-01-11 14:00:23 +0800

    adding and fetching data from database

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,11 @@ function App() {
   return (
     <><Nav/>
     <LogIn/>
-  
+    <Routes>
+      <Route path="/" element={<Dashboard />}/>
+      <Route path="/courselist" element={<CourseList/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+    </Routes>
       <Footer/>
     </>
   );
