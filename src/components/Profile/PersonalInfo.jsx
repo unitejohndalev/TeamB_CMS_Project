cc5cbf64901473667f5801870ef9a22e0d7cdd7a
John <unitejohndalev@gmail.com>
2024-01-17 13:51:48 +0800

    Profile UI, Responsiveness & Functionalities

--- src/components/Profile/PersonalInfo.jsx
+++ src/components/Profile/PersonalInfo.jsx
@@ -1,9 +1,13 @@
+//january 12 2024
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { NavLink } from "react-router-dom";
+
+//import logo for profile pic
+import profilePic from "../../assets/Registration.svg";
 
 const PersonalInfo = () => {
   const [instructors, setInstructors] = useState([]);
+
   const [instructor, setInstructor] = useState({
     instructor_first_name: "",
     instructor_last_name: "",
@@ -29,53 +33,49 @@ const PersonalInfo = () => {
     await axios.post("http://localhost:8080/instructor", instructor);
   };
 
+  console.log(instructors);
   const { first_name, last_name, email, username } = instructor;
-
   return (
-   
-      <div className="box-outer p-4">
-        {/* Inner Box */}
-        <div className="w-[90%] flex flex-col m-auto h-full lg:w-[700px]  p-6">
-          {/* Navigation Tabs */}
-          <div className="flex gap-5 mt-5  ">
-            {/* <NavLink to="/personal-info" activeClassName="active-tab"> */}
-            {/* </NavLink> */}
-            {/* Add more tabs as needed */}
-          </div>
+    <>
+      <div className="relative w-[90%] flex flex-col justify-end items-start h-full  lg:w-[700px] lg:h-[480px] xl:w-[979px] xl:h-[702px] bg-[#126912] lg:rounded-md shadow-md">
+        <p className="absolute top-0  text-white lg:text-[1.5rem] xl:text-[32px] lg:italic lg:pl-5">
+          Personal Information
+        </p>
+        <form
+          onSubmit={(e) => handleSubmit(e)}
+          className="absolute bottom-[-.3rem] flex flex-col h-full gap-y-5 w-[90%] lg:h-[450px]  lg:w-[680px]  xl:h-[655px] xl:w-[948px] bg-[#BCE8B1] lg:rounded-b-md lg:rounded-tr-md shadow-md">
+          <div className="lg:w-[90%] lg:m-auto">
+            <div className="lg:flex lg:w-[100%] lg:gap-x-5">
+              <div className="lg:w-[30%]">
+                <img
+                  src={profilePic}
+                  alt=""
+                  className="hidden lg:flex lg:w-[200px] lg:h-[150px] xl:w-[292px] xl:h-[239px]"
+                />
+              </div>
+              <div className="lg:flex lg:flex-col lg:w-[70%] xl:gap-y-10 lg:gap-y-5">
+                <input
+                  className="input-style lg:w-full lg:mt-10 xl:mt-20"
+                  placeholder="First Name * "
+                  type="text"
+                  name="instructor_first_name" //should be edited
+                  value={first_name}
+                  onChange={(e) => handleInputChange(e)}
+                />
 
-          {/* Content */}
-          <form
-            onSubmit={(e) => handleSubmit(e)}
-            className="flex flex-col h-full gap-y-5 w-full m-auto"
-          />
-          <div className="w-[90%] flex flex-col m-auto h-full shadow-[10px_-20px_1px_10px_rgba(0,100,0,.75)] lg:w-[700px]">
-            <p className="text-white">Personal Information</p>
-            <form
-              onSubmit={(e) => handleSubmit(e)}
-              className="flex flex-col h-full gap-y-5 w-[90%] m-auto "
-            >
-              <img src="" alt="" />
-
+                <input
+                  className="input-style lg:w-full"
+                  placeholder="Last Name * "
+                  type="text"
+                  name="instructor_last_name" //edit
+                  value={last_name}
+                  onChange={(e) => handleInputChange(e)}
+                />
+              </div>
+            </div>
+            <div className="lg:flex lg:gap-y-5 lg:flex-col xl:gap-y-10">
               <input
-                className="input-style"
-                placeholder="First Name * "
-                type="text"
-                name="instructor_first_name" //should be edited
-                value={first_name}
-                onChange={(e) => handleInputChange(e)}
-              />
-
-              <input
-                className="input-style"
-                placeholder="Last Name * "
-                type="text"
-                name="instructor_last_name" //edit
-                value={last_name}
-                onChange={(e) => handleInputChange(e)}
-              />
-
-              <input
-                className="input-style"
+                className=" input-style lg:mt-5"
                 placeholder="Username * "
                 type="text"
                 name="instructor_username" //edit
@@ -83,21 +83,26 @@ const PersonalInfo = () => {
                 onChange={(e) => handleInputChange(e)}
               />
               <input
-                className="input-style"
+                className=" input-style"
                 placeholder="Email Address * "
                 type="text"
                 name="instructor_email" //edit
                 value={email}
                 onChange={(e) => handleInputChange(e)}
               />
-
-              <button className="mb-5 btn-style w-full m-auto">Update</button>
-            </form>
+              <div className="lg:w-[100%] lg:flex lg:justify-end">
+                <button className="w-full mb-5 lg:mb-0 lg:mt-5 btn-style lg:w-[120px] lg:flex lg:justify-center xl:w-[170px] rounded-full ">
+                  Update
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
+        </form>
       </div>
-
+    </>
   );
 };
 
 export default PersonalInfo;
+
+//january 12 2024
