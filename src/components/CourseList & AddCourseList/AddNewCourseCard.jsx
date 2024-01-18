d6978e1b2e142814c84aeca90f473a70c7762ed8
Gem Ramirez <geraldinemaesramirez@gmail.com>
2024-01-18 11:32:33 +0800

    add new course center title

--- src/components/CourseList & AddCourseList/AddNewCourseCard.jsx
+++ src/components/CourseList & AddCourseList/AddNewCourseCard.jsx
@@ -50,7 +50,7 @@ const AddNewCourseCard = () => {
     <div>
       {/* Content */}
       <div className="mt-5 w-full h-[100vh] ">
-        <div className="m-5 text-black lg:font-bold lg:text-3xl py-1 lg:py-0 lg:text-[1.2rem] lg:w-[70%] flex justify-center items-center">
+        <div className="m-5 text-black lg:font-bold lg:text-3xl py-1 lg:py-0 lg:text-[1.2rem] lg:w-[98%] flex justify-center items-center">
           <p className="lg:font-bold">Create New Course</p>
         </div>
         <div className="mb-.5 hidden lg:flex lg:border-b-8 lg:rounded-lg lg:border-b-[#BCE8B1] lg:w-[70%] m-auto"></div>{" "}
