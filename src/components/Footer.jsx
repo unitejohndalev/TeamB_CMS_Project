7cc92e729200b51bd7f98bc775d3c1a6233a8a3e
John <unitejohndalev@gmail.com>
2024-01-18 11:36:55 +0800

    footer development

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div>
       <footer className="flex justify-center pt-5 pb-5">
         <div>
-          <p>2023 all rights reserved</p>
+          <p className="text-[#4D9349] font-medium">All Rights Reserved | Copyright 2024</p>
         </div>
       </footer>
     </div>
