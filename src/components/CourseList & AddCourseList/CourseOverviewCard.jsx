88b8e230ef7cf7e465caafd5629f6c963e97ca81
Gem Ramirez <geraldinemaesramirez@gmail.com>
2024-01-19 14:34:30 +0800

    </div>

--- src/components/CourseList & AddCourseList/CourseOverviewCard.jsx
+++ src/components/CourseList & AddCourseList/CourseOverviewCard.jsx
@@ -129,6 +129,7 @@ const CourseOverviewCard = () => {
 
         </div>
       </div>
+      </div>
     </>
   );
 };
--- src/components/CourseList & AddCourseList/CreateNewChapterTitle.jsx
+++ src/components/CourseList & AddCourseList/CreateNewChapterTitle.jsx
@@ -122,6 +122,7 @@ const CreateNewChapterTitle = () => {
 
         </div>
       </div>
+      </div>
     </>
   );
 };
