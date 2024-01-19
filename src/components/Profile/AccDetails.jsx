035c59c476262fefa59953cd690929641add94c5
John <unitejohndalev@gmail.com>
2024-01-19 09:49:56 +0800

    modify acc details according to test

--- src/components/Profile/AccDetails.jsx
+++ src/components/Profile/AccDetails.jsx
@@ -1,6 +1,5 @@
 import Footer from "../Footer";
 
-
 const AccDetails = () => {
   return (
     <>
@@ -12,19 +11,18 @@ const AccDetails = () => {
         </p>
         <div className="absolute bottom-[-.3rem] flex items-center flex-col h-full gap-y-5 w-[90%] lg:h-[320px]  lg:w-[680px]  xl:w-[948px] xl:h-[455px] bg-[#BCE8B1] lg:rounded-b-md lg:rounded-tr-md shadow-md">
           <div className="xl:w-[90%] flex flex-col items-center mt-10 xl:mt-7 lg:gap-y-[2rem] xl:gap-y-[3rem]">
-            <p className="lg:w-[600px] lg:h-[50px] xl:w-[829px] xl:h-[70px] bg-[#D1DFCD] rounder-[5px]  text-[#4D4141] text-opacity-[53%] shadow-lg flex items-center pl-2 ">
-              Email
+            <p className="relative lg:w-[600px] lg:h-[50px] xl:w-[829px] xl:h-[70px] bg-[#D1DFCD] lg:text-[.8rem] 2xl:text-[16px] lg:rounded-lg 2xl:rounder-[5px] text-[#4D4141] text-opacity-[53%] shadow-lg  flex items-center pl-2 ">
+              <span className="absolute top-1">Email</span>
             </p>
-            <p className=" lg:w-[600px] lg:h-[50px] xl:w-[829px] xl:h-[70px] bg-[#D1DFCD] rounder-[5px] text-[#4D4141] text-opacity-[53%] shadow-lg  flex items-center pl-2 ">
-              Username
+            <p className="relative lg:w-[600px] lg:h-[50px] xl:w-[829px] xl:h-[70px] bg-[#D1DFCD] lg:text-[.8rem] 2xl:text-[16px] lg:rounded-lg 2xl:rounder-[5px] text-[#4D4141] text-opacity-[53%] shadow-lg  flex items-center pl-2 ">
+              <span className="absolute top-1">Username</span>
             </p>
-            <p className=" lg:w-[600px] lg:h-[50px] xl:w-[829px] xl:h-[70px] bg-[#D1DFCD] rounder-[5px] text-[#4D4141] text-opacity-[53%] shadow-lg  flex items-center pl-2 ">
-              Password
+            <p className="relative lg:w-[600px] lg:h-[50px] xl:w-[829px] xl:h-[70px] bg-[#D1DFCD] lg:text-[.8rem] 2xl:text-[16px] lg:rounded-lg 2xl:rounder-[5px] text-[#4D4141] text-opacity-[53%] shadow-lg  flex items-center pl-2 ">
+              <span className="absolute top-1">Password</span>
             </p>
           </div>
         </div>
       </div>
-    
     </>
   );
 };
