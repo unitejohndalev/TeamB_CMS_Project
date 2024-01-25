17b12ad6c98ba61847eb1aba6cd15d0b50ff9703
John <unitejohndalev@gmail.com>
2024-01-25 14:28:43 +0800

    personal information and personal acc UI and Functionalities Update

--- src/components/Profile/AccDetails.jsx
+++ src/components/Profile/AccDetails.jsx
@@ -12,13 +12,14 @@ const AccDetails = () => {
         <div className="absolute bottom-[-.3rem] flex items-center flex-col h-full gap-y-5 w-[90%] lg:h-[320px]  lg:w-[680px]  xl:w-[948px] xl:h-[455px] bg-[#BCE8B1] lg:rounded-b-md lg:rounded-tr-md shadow-md">
           <div className="xl:w-[90%] flex flex-col items-center mt-10 xl:mt-7 lg:gap-y-[2rem] xl:gap-y-[3rem]">
             <p className="relative lg:w-[600px] lg:h-[50px] xl:w-[829px] xl:h-[70px] bg-[#D1DFCD] lg:text-[.8rem] 2xl:text-[16px] lg:rounded-lg 2xl:rounder-[5px] text-[#4D4141] text-opacity-[53%] shadow-lg  flex items-center pl-2 ">
-              <span className="absolute top-1">Email</span>
+              <span className="absolute top-1 text-shadow">Email</span>
             </p>
             <p className="relative lg:w-[600px] lg:h-[50px] xl:w-[829px] xl:h-[70px] bg-[#D1DFCD] lg:text-[.8rem] 2xl:text-[16px] lg:rounded-lg 2xl:rounder-[5px] text-[#4D4141] text-opacity-[53%] shadow-lg  flex items-center pl-2 ">
-              <span className="absolute top-1">Username</span>
+              <span className="absolute top-1 text-shadow">Username</span>
             </p>
-            <p className="relative lg:w-[600px] lg:h-[50px] xl:w-[829px] xl:h-[70px] bg-[#D1DFCD] lg:text-[.8rem] 2xl:text-[16px] lg:rounded-lg 2xl:rounder-[5px] text-[#4D4141] text-opacity-[53%] shadow-lg  flex items-center pl-2 ">
-              <span className="absolute top-1">Password</span>
+            <p
+              className="cursor-pointer mr-auto lg:w-[150px] text-center lg:h-[30px] font-semibold xl:w-[159px] xl:h-[36px] 2xl:w-[829px] 2xl:h-[70px] bg-[#D1DFCD] lg:text-[.8rem] 2xl:text-[16px] lg:rounded-lg 2xl:rounder-[5px] text-[#4D4141] text-opacity-[53%] shadow-lg  flex justify-center items-center text-shadow ">
+              <span className="">Change Password</span>
             </p>
           </div>
         </div>
--- src/components/Profile/PersonalInfo.jsx
+++ src/components/Profile/PersonalInfo.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unknown-property */
 //january 12 2024
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -12,7 +13,7 @@ const PersonalInfo = () => {
   const [instructor, setInstructor] = useState({
     instructor_first_name: "",
     instructor_last_name: "",
-    instructor_username: "",
+    instructor_contact_number: "",
     instructor_email: "",
   });
 
@@ -27,15 +28,60 @@ const PersonalInfo = () => {
 
   const handleInputChange = (e) => {
     setInstructor({ ...instructor, [e.target.name]: e.target.value });
+  
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    // Validate contact number
+    if (instructor.instructor_contact_number.length !== 11) {
+      // Show an error message or handle the invalid input as per your requirement
+      alert("Contact number must be exactly 11 digits.");
+      return; // Stop the submission process
+    }
+
     await axios.post("http://localhost:8080/instructor", instructor);
+
+    setErrorFirstName(false);
+    setErrorLastName(false);
+    setErrorContactNo(false);
+
+    if (instructor.instructor_first_name === "") {
+      setErrorFirstName(true);
+    }
+    if (instructor.instructor_last_name == "") {
+      setErrorLastName(true);
+    }
+    if (instructor.instructor_contact_number == "") {
+      setErrorContactNo(true);
+    }
+
+    setEditPersonalInfo(true);
   };
 
   console.log(instructors);
-  const { first_name, last_name, email, username } = instructor;
+  const {
+    instructor_first_name,
+    instructor_last_name,
+    instructor_email,
+    instructor_contact_number,
+  } = instructor;
+
+  //react hook for tooltip
+  const [showTooltipFirstName, setShowTooltipFirstName] = useState(false);
+  const [showTooltipLastName, setShowTooltipLastName] = useState(false);
+  // const [showTooltipEmail, setShowTooltipEmail] = useState(false);
+  const [showTooltipContactNo, setShowTooltipContactNo] = useState(false);
+
+ //edit update personal info
+ const [editPersonalInfo, setEditPersonalInfo] = useState(true)
+
+
+//set react hook for all text field
+  const [errorFirstName, setErrorFirstName] = useState(false);
+  const [errorLastName, setErrorLastName] = useState(false);
+  const [errorContactNo, setErrorContactNo] = useState(false);
+
   return (
     <>
       <div className="relative w-[90%] flex flex-col justify-end items-start h-full  lg:w-[700px] lg:h-[480px] xl:w-[979px] xl:h-[702px] bg-[#126912] lg:rounded-md shadow-md">
@@ -43,6 +89,8 @@ const PersonalInfo = () => {
           Personal Information
         </p>
         <form
+          autoComplete="off"
+          required
           onSubmit={(e) => handleSubmit(e)}
           className="absolute bottom-[-.3rem] flex flex-col h-full gap-y-5 w-[90%] lg:h-[450px]  lg:w-[680px]  xl:h-[655px] xl:w-[948px] bg-[#BCE8B1] lg:rounded-b-md lg:rounded-tr-md shadow-md">
           <div className="lg:w-[90%] lg:m-auto">
@@ -55,91 +103,168 @@ const PersonalInfo = () => {
                 />
               </div>
               <div className="relative lg:flex lg:flex-col lg:w-[70%] xl:gap-y-10 lg:gap-y-5">
-                <label
-                  htmlFor="firstName"
-                  className=" text-[#4D4141] text-opacity-[53%] absolute z-10 top-10 left-2 text-[.8rem] xl:text-[16px]  xl:top-[5rem]">
-                  First Name <span className="text-[#FF2626]">*</span>
-                </label>
-                <input
-                  className="relative input-style lg:w-full lg:mt-10 xl:mt-20 bg-[#EBFFE5]"
-                  id="firstName"
-                  type="text"
-                  name="instructor_first_name" //should be edited
-                  value={first_name}
-                  onChange={(e) => handleInputChange(e)}
-                  maxLength={50}
-                  onKeyPress={(e) => {
-                    // Allow only alphabets and some special characters like space
-                    const regex = /^[a-zA-Z\s]*$/;
-                    if (!regex.test(e.key)) {
-                      e.preventDefault();
+                <div className="relative">
+                  <label
+                    htmlFor="firstName"
+                    className={
+                      editPersonalInfo
+                        ? " text-[#4D4141] text-opacity-[53%] absolute xl:top-[20.9rem] xl:left-[3.5rem] z-10 top-0 left-2 text-[.8rem] xl:text-[16px]"
+                        : showTooltipLastName
+                        ? " text-[#4D4141] text-opacity-[53%] absolute z-10 top-0 left-2 text-[.8rem] xl:text-[16px]  xl:top-[5rem] blur-lg"
+                        : " text-[#4D4141] text-opacity-[53%] absolute z-10 top-0 left-2 text-[.8rem] xl:text-[16px]  xl:top-[5rem]"
+                    }>
+                    First Name <span className="text-[#FF2626]">*</span>
+                  </label>
+                  <input
+                    className={
+                      editPersonalInfo
+                        ? "input-style"
+                        : showTooltipLastName
+                        ? " relative input-style lg:w-full  bg-[#EBFFE5] blur-[.1rem]"
+                        : " relative input-style lg:w-full  bg-[#EBFFE5] "
                     }
-                  }}
-                />
-                <label
-                  htmlFor="lastName"
-                  className=" text-[#4D4141] text-opacity-[53%] absolute z-10 top-[6.7rem] left-2 text-[.8rem] xl:text-[16px] xl:top-[10.5rem] ">
-                  Last Name <span className="text-[#FF2626]">*</span>
-                </label>
-                <input
-                  className="input-style lg:w-full bg-[#EBFFE5]"
-                  id="lastName"
-                  type="text"
-                  name="instructor_last_name" //edit
-                  value={last_name}
-                  onChange={(e) => handleInputChange(e)}
-                  maxLength={50}
-                  onKeyPress={(e) => {
-                    // Allow only alphabets and some special characters like space
-                    const regex = /^[a-zA-Z\s]*$/;
-                    if (!regex.test(e.key)) {
-                      e.preventDefault();
+                    id="firstName"
+                    type="text"
+                    name="instructor_first_name" //should be edited
+                    value={instructor_first_name}
+                    onChange={(e) => handleInputChange(e)}
+                    maxLength={50}
+                    required={true}
+                    onError={errorFirstName}
+                    onKeyPress={(e) => {
+                      // Allow only alphabets and some special characters like space
+                      const regex = /^[a-zA-Z\s]*$/;
+                      if (!regex.test(e.key)) {
+                        e.preventDefault();
+                      }
+                    }}
+                    onMouseOver={() => setShowTooltipFirstName(true)}
+                    onMouseLeave={() => setShowTooltipFirstName(false)}
+                    disabled={editPersonalInfo}
+                  />
+                  {showTooltipFirstName && editPersonalInfo === false && (
+                    <div className="absolute top-[-3.5rem] left-10 bg-[#fff] w-[60%] p-1 rounded-lg border-[1px] border-[#EBFFE5]">
+                      <p className="text-[.8rem] text-[#4D4141] text-opacity-[53%]">
+                        Maximum of 50 letters, only alphabets and spaces are
+                        allowed.
+                      </p>
+                    </div>
+                  )}
+                </div>
+                <div className="relative">
+                  <label
+                    htmlFor="lastName"
+                    className=" text-[#4D4141] text-opacity-[53%] absolute z-10 top-0 left-2 text-[.8rem] xl:text-[16px] xl:top-[10.5rem] ">
+                    Last Name <span className="text-[#FF2626]">*</span>
+                  </label>
+                  <input
+                    className={
+                      editPersonalInfo
+                        ? "input-style "
+                        : "relative input-style lg:w-full bg-[#EBFFE5]"
                     }
-                  }}
-                />
+                    id="lastName"
+                    type="text"
+                    name="instructor_last_name" //edit
+                    value={instructor_last_name}
+                    onChange={(e) => handleInputChange(e)}
+                    maxLength={50}
+                    required={true}
+                    onError={errorLastName}
+                    onKeyPress={(e) => {
+                      // Allow only alphabets and some special characters like space
+                      const regex = /^[a-zA-Z\s]*$/;
+                      if (!regex.test(e.key)) {
+                        e.preventDefault();
+                      }
+                    }}
+                    onMouseOver={() => setShowTooltipLastName(true)}
+                    onMouseLeave={() => setShowTooltipLastName(false)}
+                    disabled={editPersonalInfo}
+                  />
+                  {showTooltipLastName && editPersonalInfo === false && (
+                    <div className="absolute top-[-3.5rem] left-10 bg-[#fff] w-[60%] p-1 rounded-lg border-[1px] border-[#EBFFE5]">
+                      <p className="text-[.8rem] text-[#4D4141] text-opacity-[53%]">
+                        Maximum of 50 letters, only alphabets and spaces are
+                        allowed.
+                      </p>
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
             <div className="lg:flex lg:gap-y-5 lg:flex-col xl:gap-y-10">
               <label
                 htmlFor="Email"
-                className=" text-[#4D4141] text-opacity-[53%] absolute xl:top-[20.9rem] xl:left-[3.5rem] z-10 top-[13.2rem] left-[2.7rem] text-[.8rem] xl:text-[16px]">
+                className={
+                  editPersonalInfo
+                    ? " text-[#4D4141] text-opacity-[53%] absolute xl:top-[20.9rem] xl:left-[3.5rem] z-10 top-[13.2rem] left-[2.7rem] text-[.8rem] xl:text-[16px]"
+                    : showTooltipContactNo
+                    ? " text-[#4D4141] text-opacity-[53%] absolute xl:top-[20.9rem] xl:left-[3.5rem] z-10 top-[13.2rem] left-[2.7rem] text-[.8rem] xl:text-[16px] blur-lg"
+                    : " text-[#4D4141] text-opacity-[53%] absolute xl:top-[20.9rem] xl:left-[3.5rem] z-10 top-[13.2rem] left-[2.7rem] text-[.8rem] xl:text-[16px]"
+                }>
                 Email Address
               </label>
               <input
-                className=" input-style lg:mt-5"
+                className={
+                  showTooltipContactNo
+                    ? " input-style lg:mt-5 blur-[.01rem]"
+                    : " input-style lg:mt-5"
+                }
                 id="Email"
-                type="text"
+                type="number"
                 name="instructor_username" //edit
-                value={username}
+                value={instructor_email}
+                onChange={(e) => handleInputChange(e)}
                 disabled
-                onChange={(e) => handleInputChange(e)}
               />
-              <label
-                htmlFor="Username"
-                className=" text-[#4D4141] text-opacity-[53%] absolute z-10 top-[17.4rem] left-[2.7rem] text-[.8rem] xl:text-[16px] xl:top-[26.5rem] xl:left-[3.5rem]">
-                Contact Number <span className="text-[#FF2626]">*</span>
-              </label>
+              <div className="relative">
+                <label
+                  htmlFor="ContactNumber"
+                  className=" text-[#4D4141] text-opacity-[53%] absolute z-10 top-0 left-2 text-[.8rem] xl:text-[16px] xl:top-[26.5rem] xl:left-[3.5rem]">
+                  Contact Number <span className="text-[#FF2626]">*</span>
+                </label>
 
-              <input
-                className=" input-style bg-[#EBFFE5] placeholder:text-[#4D4141] placeholder:text-opacity-[53%]"
-                placeholder="+63"
-                type="text"
-                id="Username"
-                name="instructor_number" //edit
-                value={email}
-                onChange={(e) => handleInputChange(e)}
-                maxLength={11}
-                onKeyPress={(e) => {
-                  // Allow only numeric input
-                  const isNumeric = /^[0-9\b]+$/;
-                  if (!isNumeric.test(e.key)) {
-                    e.preventDefault();
+                <input
+                  className={
+                    editPersonalInfo
+                      ? "input-style "
+                      : " input-style bg-[#EBFFE5] placeholder:text-[#4D4141] placeholder:text-opacity-[53%]"
                   }
-                }}
-              />
+                  placeholder="+63"
+                  type="text"
+                  id="ContactNumber"
+                  name="instructor_contact_number" //edit
+                  value={instructor_contact_number}
+                  onChange={(e) => handleInputChange(e)}
+                  maxLength={11}
+                  required={true}
+                  onError={errorContactNo}
+                  onKeyPress={(e) => {
+                    // Allow only numeric input
+                    const isNumeric = /^[0-9\b]+$/;
+                    if (!isNumeric.test(e.key)) {
+                      e.preventDefault();
+                    }
+                  }}
+                  disabled={editPersonalInfo}
+                  onMouseOver={() => setShowTooltipContactNo(true)}
+                  onMouseLeave={() => setShowTooltipContactNo(false)}
+                />
+                {showTooltipContactNo && editPersonalInfo === false && (
+                  <div className="absolute top-[-3.5rem] left-10 bg-[#fff] w-[40%] p-1 rounded-lg border-[1px] border-[#EBFFE5]">
+                    <p className="text-[.8rem] text-[#4D4141] text-opacity-[53%]">
+                      Maximum of 11 numbers, only numbers are allowed.
+                    </p>
+                  </div>
+                )}
+              </div>
               <div className="lg:w-[100%] lg:flex lg:justify-end">
-                <button className="w-full mb-5 lg:mb-0 lg:mt-5 btn-style lg:w-[120px] lg:flex lg:justify-center xl:w-[170px] rounded-full ">
-                  Update
+                <button
+                  type="submit"
+                  onClick={() => setEditPersonalInfo(false)}
+                  className="w-full mb-5 lg:mb-0 lg:mt-5 btn-style lg:w-[120px] lg:flex lg:justify-center xl:w-[170px] rounded-full ">
+                  {editPersonalInfo ? "Edit" : "Update"}
                 </button>
               </div>
             </div>
