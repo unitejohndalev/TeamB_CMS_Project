c4199429f608cf106ba5d66c385723d8b5526bec
CedrickDale <cdalesunga@gmail.com>
2024-02-13 14:20:15 +0800

    Responsiveness

--- Frontend/src/components/Topic/TopicPages/TopicPage.jsx
+++ Frontend/src/components/Topic/TopicPages/TopicPage.jsx
@@ -1,14 +1,8 @@
-/* eslint-disable react/prop-types */
-//2/2/2024 junite, develop edit page
-//2/3/2024 junite, continuation of development, completed
-
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { IoArrowBackCircle } from "react-icons/io5";
 import { IoIosAddCircle } from "react-icons/io";
-
 import { MdDelete } from "react-icons/md";
-
 import DeleteTopicModal from "../TopicModal/DeleteTopicModal";
 import AddTopic from "./AddTopic";
 import EditTopic from "./EditTopic";
@@ -21,46 +15,38 @@ const TopicPage = () => {
     navigate(-1);
   };
 
-  //delete modal state
   const [deleteModalVisible, setDeleteModalVisible] = useState(false);
-
-  //course description state
-  const [showCourseDescription, setShowCourseDescription] = useState(true)
-  
-  //add topic state
-  const [showAddTopic, setShowAddTopic] = useState(false)
-
-  //edit topic state
+  const [showCourseDescription, setShowCourseDescription] = useState(true);
+  const [showAddTopic, setShowAddTopic] = useState(false);
   const [showEditTopic, setShowEditTopic] = useState(false);
 
-  //functions for showing and hiding description, add topic, and edit topic
- 
   const showDescriptionHandle = () => {
-    setShowCourseDescription(true)
-    setShowAddTopic(false)
-    setShowEditTopic(false)
-  }
-    const showAddHandle = () => {
-      setShowAddTopic(true);
-      setShowCourseDescription(false);
-      setShowEditTopic(false);
-    };
+    setShowCourseDescription(true);
+    setShowAddTopic(false);
+    setShowEditTopic(false);
+  };
 
-       const showEditHandle = () => {
-         setShowEditTopic(true);
-         setShowCourseDescription(false);
-         setShowAddTopic(false);
-       };
+  const showAddHandle = () => {
+    setShowAddTopic(true);
+    setShowCourseDescription(false);
+    setShowEditTopic(false);
+  };
 
+  const showEditHandle = () => {
+    setShowEditTopic(true);
+    setShowCourseDescription(false);
+    setShowAddTopic(false);
+  };
 
   return (
     <>
       <div className="flex mt-[80px] lg:h-[100vh] 2xl:h-[1011px]">
         {/* sidebar */}
-        <div className="h-full flex flex-col items-center lg:w-[250px] 2xl:w-[375px] bg-[#126912]">
+        <div className="h-full flex flex-col items-center lg:w-[250px] xl:w-[300px] bg-[#126912]">
           <div
-            className="flex justify-start  pt-3 pb-8 cursor-pointer w-[90%]"
-            onClick={goBack}>
+            className="flex justify-start pt-3 pb-8 cursor-pointer w-[90%]"
+            onClick={goBack}
+          >
             <span className="text-[2.1rem] text-white">
               <IoArrowBackCircle />
             </span>
@@ -70,54 +56,62 @@ const TopicPage = () => {
           <div className="h-[70%] w-[80%] ">
             <div>
               <p
-                className="cursor-pointer font-light pb-3 text-white TeamB_text-shadow text-[1.3rem] 2xl:text-[32px]"
-                onClick={showDescriptionHandle}>
+                className="cursor-pointer font-light pb-3 text-white TeamB_text-shadow text-[1.3rem] lg:text-xl"
+                onClick={showDescriptionHandle}
+              >
                 Description
               </p>
             </div>
             <div className="flex items-center justify-between">
               <p
-                className="cursor-pointer py-2 font-light text-white TeamB_text-shadow text-[1.2rem] 2xl:text-[32px]"
-                onClick={showEditHandle}>
+                className="cursor-pointer py-2 font-light text-white TeamB_text-shadow text-[1.2rem] lg:text-lg"
+                onClick={showEditHandle}
+              >
                 Topic 1: Title
               </p>
               <span
-                className="text-[1.5rem] text-white cursor-pointer"
-                onClick={() => setDeleteModalVisible((prev) => !prev)}>
+                className="text-lg text-white cursor-pointer"
+                onClick={() => setDeleteModalVisible((prev) => !prev)}
+              >
                 <MdDelete />
               </span>
             </div>
             <div className="flex items-center justify-between">
               <p
-                className="py-2 cursor-pointer font-light text-white TeamB_text-shadow text-[1.2rem] 2xl:text-[32px]"
-                onClick={showEditHandle}>
+                className="py-2 cursor-pointer font-light text-white TeamB_text-shadow text-[1.2rem] lg:text-lg"
+                onClick={showEditHandle}
+              >
                 Topic 2: Title
               </p>
               <span
-                className="text-[1.5rem] text-white cursor-pointer"
-                onClick={() => setDeleteModalVisible((prev) => !prev)}>
+                className="text-lg text-white cursor-pointer"
+                onClick={() => setDeleteModalVisible((prev) => !prev)}
+              >
                 <MdDelete />
               </span>
             </div>
             <div className="flex items-center justify-between">
               <p
-                className=" cursor-pointer py-2 font-light text-white TeamB_text-shadow text-[1.2rem] 2xl:text-[32px]"
-                onClick={showEditHandle}>
+                className=" cursor-pointer py-2 font-light text-white TeamB_text-shadow text-[1.2rem] lg:text-lg"
+                onClick={showEditHandle}
+              >
                 Topic 3: Title
               </p>
               <span
-                className="text-[1.5rem] text-white cursor-pointer"
-                onClick={() => setDeleteModalVisible((prev) => !prev)}>
+                className="text-lg text-white cursor-pointer"
+                onClick={() => setDeleteModalVisible((prev) => !prev)}
+              >
                 <MdDelete />
               </span>
             </div>
             <div
               className="flex items-center h-[30%] mt-20 "
-              onClick={showAddHandle}>
+              onClick={showAddHandle}
+            >
               <div className="text-white text-[2.5rem] pr-2 cursor-pointer">
                 <IoIosAddCircle />
               </div>
-              <span className="font-medium text-white cursor-pointer text-[1rem] 2xl:text-[24px]">
+              <span className="font-medium text-white cursor-pointer text-[1rem] lg:text-lg">
                 Add New Topic
               </span>
             </div>
